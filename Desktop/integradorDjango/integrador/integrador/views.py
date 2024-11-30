39b801ef009865a1c6427ff73c90b63ab08cd7d3
Gerson Emanuel <142825194+GersonEmanuel@users.noreply.github.com>
2024-11-30 13:04:41 -0300

    Delete Desktop/integradorDjango directory
    wrong directory pushed

--- Desktop/integradorDjango/integrador/integrador/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-
-def home_page(request):
-    context = {'title': 'Pagina Principal', 'content':'principal'}
-    return render(request, 'index.html')
-
-def pagina_cadastro(request):
-    return render(request, 'cadastro.html')
