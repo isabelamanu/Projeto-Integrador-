ce45af261647f694cefc0b0169366499fedc0946
gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-30 12:56:48 -0300

    integDjango

--- integradorDjango/torneio/models.py
+++ integradorDjango/torneio/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Torneio(models.Model):
+    round = models.JSONField(default=list)
+    round2 = models.JSONField(default=list)
+    final = models.JSONField(default=list)
+    campeao = models.CharField(max_length=60, blank= True)
+
+    def __str__(self) -> str:
+        return f"Torneio {self.id}"
