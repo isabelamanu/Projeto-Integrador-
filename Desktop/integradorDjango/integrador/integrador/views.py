c64834736ddebe717070ff785a34e5f20900b498
gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-29 23:20:34 -0300

    base

---
+++ Desktop/integradorDjango/integrador/integrador/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+def home_page(request):
+    context = {'title': 'Pagina Principal', 'content':'principal'}
+    return render(request, 'index.html')
+
+def pagina_cadastro(request):
+    return render(request, 'cadastro.html')
