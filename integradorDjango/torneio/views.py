94464e6e94193347e25d04dbb28995c5f5a04938
gerson <gerson.emanuel@escolar.ifrn.edu.br>
2024-11-30 18:24:47 -0300

    Class based view

--- integradorDjango/torneio/views.py
+++ integradorDjango/torneio/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, FormView, View
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from .models import Torneio
 
 # Create your views here.
+'''
+FBV (function based view)
+prefiro class based view, mas sao a mesma coisa
 
 def quantidade_jogadores(request):
     if request.method == 'POST':
@@ -19,3 +24,16 @@ def pagina_cadastro(request):
 
 def chaveamento(request):
     return render(request, 'chaveamento.html')
+'''
+
+# CBV (class based view)
+
+class QuantidadeJogadoresView(View):
+    template_name = "numero_jogadores.html"
+
+    def post(self, request, *args, **kwargs):
+        numero = int(request.POST.get('numero'))
+        return render(request, 'adicionar_jogadores.html', {'numero': numero})
+    
+
+
